2659f07b16776270460f6e4f0998902d2935874f
Kevin <kvergara1@ggc.edu>
2023-02-28 00:28:17 -0500

    done

---
+++ src/Exercise1.java
@@ -0,0 +1,32 @@
+/**
+ * @class: Exercise1
+ * @author: Kevin Vergara
+ * @verison 1.0
+ * @course: ITEC 2140 - 04, Spring 20023
+ * @written: February 27, 2023
+ * description: this program asks the user to enter a positive number to find out if it is a prime number or not
+ * the program uses a while loop to evaluate the integer and a boolean value for the if else statement to print
+ * the integer and whether it is prime or not. the while loops executes until the count is higher than the integer
+ * being divided by 2 to make sure no other value will leave the remainder of 0
+ */
+import java.util.Scanner;
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.print("Enter a positive integer: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int count = 2;
+        boolean no = false;
+        while (count <= number / 2) {
+            if (number % count == 0) {
+                no = true;
+            }
+            count++;
+        }
+        if(no == false){
+            System.out.println(number + " is a prime number");
+        } else{
+            System.out.println(number + " is not a prime number");
+        }
+    }
+}
---
+++ src/Exercise2.java
@@ -0,0 +1,27 @@
+/**
+ * @class: Exercise2
+ * @author: Kevin Vergara
+ * @verison 1.0
+ * @course: ITEC 2140 - 04, Spring 20023
+ * @written: February 27, 2023
+ * description: this program allows the user to enter as many scores as they want. once they have entered
+ * as many scores as they want they type in an integer which causes the hasNextInt method to be false
+ * thus stopping the while loop from running. it prints the average afterwards
+ *
+ */
+import java.util.Scanner;
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        System.out.print("Enter as many scores you like when you are done enter any character:");
+        while(input.hasNextInt()){
+            System.out.print("Enter as many scores you like when you are done enter any character:");
+            int grade = input.nextInt();
+            sum += grade;
+            count++;
+            }System.out.println((sum / count) + " is your average");
+        }
+    }
+
